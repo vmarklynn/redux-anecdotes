79e9ad237ef5a393b9b6196088dda217dd0e96fa
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2024-08-01 22:53:17 -0700

    Refactored component AnecdoteList to its own component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,12 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { voteAction, createAction } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes)
-  )
-  const dispatch = useDispatch()
-
-  const vote = (id) => {
-    dispatch(voteAction(id))
-  }
-
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList />
       <AnecdoteForm />
     </div>
   )
