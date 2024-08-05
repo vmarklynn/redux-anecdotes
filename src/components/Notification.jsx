116c8422275d716402d40e15396e5b6e5d897f58
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2024-08-04 22:54:38 -0700

    Modified to display initial value

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -2,12 +2,7 @@ import { createNotification } from "../reducers/notificationReducer"
 import { useDispatch, useSelector } from "react-redux"
 
 const Notification = () => {
-  const dispatch = useDispatch()
-
   const notification = useSelector(state => state.notification)
-
-  dispatch(createNotification('TEST THIS IS AN ALERT'))
-
 
   const style = {
     border: 'solid',
--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -2,11 +2,12 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const notificationSlice = createSlice({
   name: 'notification',
-  initialState: '',
+  initialState: 'TEST VALUE',
   reducers: {
     createNotification(state, action) {
       return action.payload
     }
+
   }
 })
 
